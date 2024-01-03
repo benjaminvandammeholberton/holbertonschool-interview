0098acdd722bee4338bcfe5ef87ae26c05760389
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-01-03 17:19:38 +0100

    Fix: pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,12 +6,14 @@ Module: unlock_boxes
 This module provides a function to determine if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     Determine if all the boxes can be opened.
 
     Args:
-    - boxes (List[List[int]]): A list of lists where each sublist represents a box and contains keys to other boxes.
+    - boxes (List[List[int]]): A list of lists where each sublist\
+        represents a box and contains keys to other boxes.
 
     Returns:
     - bool: True if all boxes can be opened, else False.
@@ -38,7 +40,8 @@ def canUnlockAll(boxes):
 
         # Explore each key in the current box
         for key in keys:
-            # If the key opens a box that hasn't been opened yet, recursively explore that box
+            # If the key opens a box that hasn't been opened yet,
+            #recursively explore that box
             if key < len(boxes) and key not in opened_boxes:
                 dfs(key)
 
