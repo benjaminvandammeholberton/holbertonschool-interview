ef5e6ffad36b2d5c679f2b87be1e50b86a4e625b
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-01-10 17:23:59 +0100

    Fix: task 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -18,6 +18,9 @@ listint_t *insert_node(listint_t **head, int number)
 
     current = *head;
 
+    if (number == NULL)
+        return NULL;
+
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
