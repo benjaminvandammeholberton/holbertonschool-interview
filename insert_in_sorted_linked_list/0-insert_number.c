5acbf921964ed58e45cb858db125d61c5dabbf0b
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-01-12 11:12:55 +0100

    fix: task 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,45 +1,51 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
 /**
- * insert_node - Inserts a new node with a given number into a
- * sorted linked list.
- *
- * @head: A pointer to the head of the linked list.
- * @number: The value to be inserted into the linked list.
- *
- * Return: A pointer to the newly inserted node, or NULL on failure.
+ * insert_node - insert the node into a sorted singly linked list.
+ * @head: pointer to a linked list
+ * @number: the number of a new node
+ * Return: NULL if it failed, otherwise new node address
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *current, *new;
-
-    current = *head;
+    listint_t *new;
+    listint_t *prev = *head;
+    listint_t *curr = *head;
 
     new = malloc(sizeof(listint_t));
     if (new == NULL)
-        return NULL;
-
+        return (NULL);
     new->n = number;
-
     if (*head == NULL)
     {
         new->next = NULL;
         *head = new;
         return (new);
     }
-    else if ((*head)->n > number)
+    while (curr->next)
     {
-        new->next = *head;
-        *head = new;
-        return (new);
+        if (curr->n > number)
+        {
+            if (curr == *head)
+            {
+                new->next = curr;
+                *head = new;
+                return (new);
+            }
+            else
+            {
+                prev->next = new;
+                new->next = curr;
+                return (new);
+            }
+        }
+        prev = curr;
+        curr = curr->next;
     }
-    else
-    {
-        while (current->next != NULL && number > current->next->n)
-            current = current->next;
-        new->next = current->next;
-        current->next = new;
-    }
+    curr->next = new;
+    new->next = NULL;
     return (new);
 }
