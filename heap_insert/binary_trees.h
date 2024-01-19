51dcd360ab0134e3e3c97dc41ed47c255d4d011f
Benjamin Vandamme <6206@holbertonstudents.com>
2024-01-19 19:02:55 +0100

    Fix: task 1

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -2,6 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,17 +13,23 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
-typedef struct binary_tree_s heap_t;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *swap_value(heap_t *parent, heap_t *child);
+int get_size(heap_t *tree);
+heap_t *get_parent(heap_t *tree, int size, int index);
 
 #endif /* _BINARY_TREES_H_ */
