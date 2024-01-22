76ecc72fca9b5ffc151c94f4174eac829010233c
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-01-22 14:34:00 +0100

    add: task 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script defines a function minOperations that calculates\
+the sum of prime factors of a given number.
+"""
+
+def minOperations(n):
+    """
+    Calculate the sum of prime factors of a given number.
+    
+    Parameters:
+    - n (int): The input number
+    
+    Returns:
+    - int: The sum of prime factors
+    """
+    res = 0
+    i = 2
+    while i <= n:
+        while n % i == 0:
+            res += i
+            n = n // i
+        i += 1
+    return res
