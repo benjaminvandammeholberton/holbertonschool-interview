48ffabb9fd12f35f8051f16d3f58cf4ddfd9b33f
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-02 10:01:12 +0100

    Fix task 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -15,11 +15,28 @@ def minOperations(n):
     Returns:
     - int: The sum of prime factors
     """
-    res = 0
-    i = 2
-    while i <= n:
-        while n % i == 0:
-            res += i
-            n = n / i
-        i += 1
-    return res
+    if n <= 1:
+        return 0
+    # Set the minimum operation variable to 0
+    min_operations = 0
+    # Set the current length variable to 1 as we start with 1 character 'H'
+    current_length = 1
+    # Set the clipboard variable to 0 as we are not copying anything yet
+    clipboard = 0
+
+    # Loop until current length is equal to n
+    while current_length < n:
+        # If n is divisible by current length, we can copy all
+        if n % current_length == 0:
+            # This is the only time we can copy
+            clipboard = current_length
+            # Minimum operation is incremented by 1 because we copied
+            min_operations += 1
+
+        # Paste the clipboard
+        current_length += clipboard
+        # Minimum operation is incremented by 1 because we pasted
+        min_operations += 1
+
+    # Return minimum operation
+    return min_operations
