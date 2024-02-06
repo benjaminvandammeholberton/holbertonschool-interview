e0c4af7f2a6aeded3ac6b2472aded5d06748d74e
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-06 10:57:55 +0100

    Fix: task 0

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * is_palindrome - function that checks if a singly linked list is a palyndrome
+ *
+ * @head: pointer to pointer on the head of the singly linked list
+ *
+ * Return: 1 if it's a palyndrom, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
 	int length = 0;
-	int *array;
+	listint_t *front, *end;
+	listint_t *temp = *head;
 	int i = 0;
 	int j;
-	listint_t *temp = *head;
 
 	if (!*head)
-	{
 		return (1);
-	}
 	while (temp != NULL)
 	{
 		length += 1;
 		temp = temp->next;
 	}
-	array = (int *)malloc(length * sizeof(int));
-	if (!array)
-		return 0;
-	while (*head != NULL)
+	while (i != length / 2)
 	{
-		array[i] = (*head)->n;
-		*head = (*head)->next;
+		front = end = *head;
+		for (j = 0; j < i; j++)
+			front = front->next;
+		for (j = 0; j < length - (i + 1); j++)
+			end = end->next;
+		if (front->n != end->n)
+			return (0);
 		i++;
 	}
-	j = length - 1;
-	for (i = 0; i < length - 1 / 2; i++)
-	{
-		if (array[i] != array[j])
-			return (0);
-		j--;
-	}
-	return 1;
+	return (1);
 }
