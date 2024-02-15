24dd76e5b0f6b312fa985e0ba48823e156a404fd
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-15 12:10:48 +0100

    add: task 0

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+pattern = (
+    r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) "
+    r"\- \[\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}\:\d{2}\.\d{6}\] "
+    r"\"GET /projects/260 HTTP/1\.1\" "
+    r"(200|301|400|401|403|404|405|500) "
+    r"(\d{0,10})\s"
+)
+
+logs = {
+    'File size': 0,
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+    }
+
+try:
+    counter = 0
+    for line in sys.stdin:
+        match = re.fullmatch(pattern, line)
+        counter += 1
+        status_code = match.group(2)
+        size = int(match.group(3))
+        logs[status_code] += 1
+        logs['File size'] += size
+
+        if counter % 10 == 0:
+
+            for key, value in logs.items():
+                if logs[key] > 0:
+                    print(f"{key}: {value}")
+                    if key != 'File size':
+                        logs[key] = 0
+
+except KeyboardInterrupt:
+    for key, value in logs.items():
+        if logs[key] > 0:
+            print(f"{key}: {value}")
+            if key != 'File size':
+                logs[key] = 0
