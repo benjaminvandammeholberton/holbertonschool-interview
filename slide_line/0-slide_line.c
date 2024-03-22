ba2f8f1a92aa9b22bd1470accb1944ae89ba4615
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-22 16:10:44 +0100

    Add: task 0

---
+++ slide_line/0-slide_line.c
@@ -0,0 +1,90 @@
+#include slide_line.h
+
+/**
+ * slide_line - Slides and merges an array of integers
+ * towards a given direction
+ * 
+ * @line: Pointer to the array of integer
+ * @size: Number of elements in @line
+ * @direction: direction that elements of @line must be slide & merge
+ *
+ * Return: 1 upon success, 0 otherwise
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t i, j;
+	int merged = 0;
+
+	if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+		return (0);
+
+	if (direction == SLIDE_LEFT)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if (line[i] == 0)
+				continue;
+			for (j = i + 1; j < size; ++j)
+			{
+				if (line[j] != 0)
+				{
+					if (line[i] == line[j] && !merged)
+					{
+						line[i] *= 2;
+						line[j] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = 0, j = 0; i < size; ++i)
+		{
+			if (line[i] != 0)
+			{
+				if (i != j)
+				{
+					line[j] = line[i];
+					line[i] = 0;
+				}
+				++j;
+			}
+		}
+	}
+	else if (direction == SLIDE_RIGHT)
+	{
+		for (i = size; i > 0; --i)
+		{
+			if (line[i - 1] == 0)
+				continue;
+			for (j = i - 1; j > 0; --j)
+			{
+				if (line[j - 1] != 0)
+				{
+					if (line[i - 1] == line[j - 1] && !merged)
+					{
+						line[i - 1] *= 2;
+						line[j - 1] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = size, j = size; i > 0; --i)
+		{
+			if (line[i - 1] != 0)
+			{
+				if (i != j)
+				{
+					line[j - 1] = line[i - 1];
+					line[i - 1] = 0;
+				}
+				--j;
+			}
+		}
+	}
+	return (1);
+}
---
+++ slide_line/slide_line.h
@@ -0,0 +1,9 @@
+#ifndef _SLIDE_LINE_H_
+#define _SLIDE_LINE_H_
+
+int SLIDE_LEFT = 0;
+int SLIDE_RIGHT = 1;
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* _SLIDE_LINE_H_ */
